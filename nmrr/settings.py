cdff930839ef04b31400fb7ef36215934a83a929
Mylene Simon <mylene.simon@nist.gov>
2021-03-21 23:01:56 -0400

    chore: add APM settings to settings

--- nmrr/settings.py
+++ nmrr/settings.py
@@ -422,3 +422,31 @@ if SERVER_URI.lower().startswith("https"):
 
     # Set x-frame options
     X_FRAME_OPTIONS = "SAMEORIGIN"
+
+# APM
+MONITORING_SERVER_URI = os.environ["MONITORING_SERVER_URI"]
+if MONITORING_SERVER_URI:
+    ELASTIC_APM = {
+        "SERVICE_NAME": os.environ["SERVER_NAME"],
+        "SERVER_URL": MONITORING_SERVER_URI,
+        # Use if APM Server requires a token
+        # 'SECRET_TOKEN': '',
+    }
+    if "elasticapm.contrib.django" not in INSTALLED_APPS:
+        INSTALLED_APPS = INSTALLED_APPS + ("elasticapm.contrib.django",)
+    if "elasticapm.contrib.django.middleware.TracingMiddleware" not in MIDDLEWARE:
+        # Make sure that it is the first middleware in the list.
+        MIDDLEWARE = (
+            "elasticapm.contrib.django.middleware.TracingMiddleware",
+        ) + MIDDLEWARE
+    if "elasticapm.errors" not in LOGGING["loggers"]:
+        # https://www.elastic.co/guide/en/apm/agent/python/current/django-support.html#django-logging
+        # Log errors from the Elastic APM module to the console (recommended)
+        set_generic_logger(LOGGING, "elasticapm.errors", "ERROR", ["console"])
+    if (
+        "elasticapm.contrib.django.context_processors.rum_tracing"
+        not in TEMPLATES[0]["OPTIONS"]["context_processors"]
+    ):
+        TEMPLATES[0]["OPTIONS"]["context_processors"].append(
+            "elasticapm.contrib.django.context_processors.rum_tracing"
+        )
